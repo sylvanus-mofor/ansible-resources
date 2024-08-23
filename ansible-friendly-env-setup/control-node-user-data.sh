f5cc3211a38b0bf9b509eecbac907e4469253b29
Sylva <sylvanuszoamo@gmail.com>
2024-08-23 15:35:28 -0300

    updated control node userdata to be compatible with amazon linux 2023

--- ansible-friendly-env-setup/control-node-user-data.sh
+++ ansible-friendly-env-setup/control-node-user-data.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
-sudo amazon-linux-extras install ansible2 -y
+# Install Ansible using dnf
+sudo dnf install -y ansible
+
+# Create an Ansible user and set the password
 sudo useradd ansible
-sudo echo ansible:ansible | chpasswd
-sudo echo "ansible ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "ansible:ansible" | sudo chpasswd
+
+# Grant the Ansible user passwordless sudo privileges
+echo "ansible ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ansible
+
+# Enable password authentication and root login in SSH
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-sudo sed -i "s/.*#PermitRootLogin yes/PermitRootLogin yes/g" /etc/ssh/sshd_config
-sudo service sshd restart
+sudo sed -i "s/#PermitRootLogin yes/PermitRootLogin yes/g" /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+# Generate SSH key for Ansible user
 sudo -u ansible ssh-keygen -t rsa -f /home/ansible/.ssh/id_rsa -q -N ""
 sudo chmod 700 /home/ansible/.ssh
 sudo chmod 600 /home/ansible/.ssh/id_rsa
-# Array of remote node IP addresses
+
+# Install expect for automating SSH key copying
+sudo dnf install -y expect
+
+# Array of host node IP addresses
 REMOTE_NODES=("172.31.42.112" "172.31.38.84") # Replace & add more IPs as needed
-sudo yum install expect -y
-# Loop through each IP address
+
+# Loop through each IP address and copy the SSH key
 for REMOTE_NODE in "${REMOTE_NODES[@]}"; do
     expect -c "
     spawn sudo -u ansible ssh-copy-id ansible@$REMOTE_NODE
     expect {
-		\"*re you sure you want to continue connecting*\" { send \"yes\r\"; exp_continue }
+        \"*re you sure you want to continue connecting*\" { send \"yes\r\"; exp_continue }
         \"*pass*\" { send \"ansible\r\"; exp_continue }
         \"*Pass*\" { send \"ansible\r\" }
     }
