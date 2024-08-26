a3ebdd39229feb3444ab36d0e7fcc85977a9b063
Sylvanus Mofor <131110415+sylvanus-mofor@users.noreply.github.com>
2024-08-26 18:19:42 -0300

    Update managed-nodes-user-data.sh

--- ansible-friendly-env-setup/managed-nodes-user-data.sh
+++ ansible-friendly-env-setup/managed-nodes-user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo useradd ansible
-sudo echo ansible:ansible | chpasswd
+sudo echo sylva:ansible | chpasswd
 sudo echo "ansible ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sudo sed -i "s/.*#PermitRootLogin yes/PermitRootLogin yes/g" /etc/ssh/sshd_config
